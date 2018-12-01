c690a3458fcad96dc7212efe5e8adcde9d6f92fc
Adrien Dorsaz <adrien@adorsaz.ch>
2018-12-01 20:24:59 +0100

    DEPRECATION WARNING: calItemUtils.jsm must no longer be imported
    DEPRECATION WARNING: calItemUtils.jsm must no longer be imported
    directly, it is already available via calUtils.jsm
    
    You may find more details about this deprecation at:
    https://bugzilla.mozilla.org/show_bug.cgi?id=905097

--- common/exchangeapi/erDeleteItem.js
+++ common/exchangeapi/erDeleteItem.js
@@ -40,7 +40,6 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
 Cu.import("resource://calendar/modules/calUtils.jsm");
-Cu.import("resource://calendar/modules/calItemUtils.jsm");
 
 Cu.import("resource://exchangecommon/ecFunctions.js");
 Cu.import("resource://exchangecommon/ecExchangeRequest.js");
